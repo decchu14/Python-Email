d43adf4d6b7ca30ab7f9f3e6f4b956569a25e7d0
decchu14 <1decchu14@gmail.com>
2020-09-10 08:32:49 +0530

    This file contains simple python script to send email and html script which acts as the body or header during sending the email

--- EmailsHTML_Script.py
+++ EmailsHTML_Script.py
@@ -3,19 +3,22 @@ from email.message import EmailMessage
 from string import Template
 from pathlib import Path  # os.path
 
+# here by using Path of pathlib route to index.html file and by using Template of string convert it into template form
 html = Template(Path('index.html').read_text())
 
 
 email = EmailMessage()
-email['from'] = 'Dashline Dsouza'
-email['to'] = '1decchu14@gmail.com'
-email['subject'] = 'you won 100 dollars!'
+email['from'] = '<NAME>'  # who is sending this email
+email['to'] = 'na@na.com'  # email id for which you want to send the email
+email['subject'] = 'you won 100 dollars!'  # body of the email
 
-email.set_content(html.substitute({'name': 'LinSoo'}), 'html')
+# here you will substitute the dynamic variables to
+email.set_content(html.substitute({'name': 'Some_Name'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('1jilinsoo14@gmail.com', '1jilinsoo14@')
+    # login to email id from which you want to send an email and password
+    smtp.login('na@na.com', 'password')
     smtp.send_message(email)
     print('all good boss')
